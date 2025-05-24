fa91324ebfd631ec85acea8dae9a8064fb98b9bb
Valeria Barbu <valeriabarbu16@gmail.com>
2025-05-24 17:09:25 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let greutatMark = 78;
+let inaltimeMark = 1.69;
+let greutatJohn = 92;
+let inaltimeJohn = 1.95;
+
+let BMIMark = greutatMark/(inaltimeMark**2);
+let BMIJohn = greutatJohn/(inaltimeJohn**2);
+
+console.log ("BMI-ul lui Mark este "+BMIMark);
+console.log ("BMI-ul lui John este "+BMIJohn);
+
+let markHigherBMI = (BMIMark>BMIJohn);
+console.log (markHigherBMI);
+
+console.log ("IMCul lui Mark este mai mare ca acel al lui John.")
+console.log (`IMCul lui Mark" (${BMIMark}) este mai mare ca IMCul lui John (${BMIJohn})`)
